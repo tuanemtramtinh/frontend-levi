e095ead277a7cddbd0443e3219b4209676c229fa
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2025-11-05 12:58:57 +0700

    fix: tab ui fix

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,18 +1,54 @@
-import { AnimatedTabBar } from "@/components/AnimatedTabBar";
+import { CompassIcon } from "@/assets/Icons/CompassIcon";
+import { HomeIcon } from "@/assets/Icons/HomeIcon";
+import { PlusIcon } from "@/assets/Icons/PlusIcon";
+import { COLORS } from "@/constants/Colors";
 import { Tabs } from "expo-router";
+import styled from "styled-components/native";
+
+const CreateButton = styled.TouchableOpacity`
+    width: 42px;
+    height: 42px;
+    border-radius: 21px;
+    background-color: ${COLORS.DARKGREEN};
+    align-items: center;
+    justify-content: center;
+    margin-bottom: -5px;
+`;
 
 export default function HomeLayout() {
     return (
-        <Tabs screenOptions={{ headerShown: false }} tabBar={(props) => <AnimatedTabBar {...props}/>}>
+        <Tabs 
+            screenOptions={{ 
+                headerShown: false,
+                tabBarActiveTintColor: COLORS.DARKGREEN,
+                tabBarInactiveTintColor: COLORS.GRAY,
+                tabBarStyle: {
+                    height: 70,
+                    paddingTop: 10,
+                    backgroundColor: COLORS.PUREWHITE
+                },
+                animation: 'shift'
+            }}
+        >
             <Tabs.Screen 
                 name="index"
                 options={{
                     title: 'Nhà',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <HomeIcon color={focused ? COLORS.DARKGREEN : COLORS.GRAY} />
+                    ),
                 }}
             />
             <Tabs.Screen
                 name="create"
-                options={{}}
+                options={{
+                    tabBarLabel: () => null,
+                    tabBarIcon: () => (
+                        <CreateButton>
+                            <PlusIcon/>
+                        </CreateButton>
+                    )
+                }}
                 listeners={{
                     tabPress: (e) => {e.preventDefault();}
                 }}
@@ -21,6 +57,9 @@ export default function HomeLayout() {
                 name="tours"
                 options={{
                     title: 'Tour của tôi',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <CompassIcon color={focused ? COLORS.DARKGREEN : COLORS.GRAY} />
+                    ),
                 }}
             />
         </Tabs>
